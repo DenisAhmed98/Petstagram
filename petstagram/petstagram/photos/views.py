f1a19b63b82ff9d64e5afabb1497d2dd76d67fc2
Denis <denis.ahmed98@gmail.com>
2025-06-16 23:15:10 +0300

    WorkShop 001

---
+++ petstagram/petstagram/photos/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def add_photos(request):
+    return render(request, template_name='photos/photo-add-page.html')
+
+
+def photo_details(request):
+    return render(request, template_name='photos/photo-details-page.html')
+
+
+def photo_edit(request):
+    return render(request, template_name='photos/photo-edit-page.html')
--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'petstagram.photos',
+    'petstagram.pets',
+    'petstagram.accounts',
+    'petstagram.common',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
